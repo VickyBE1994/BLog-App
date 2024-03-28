480c4aedc52688d39fef297251156ddc11877d69
VickyBE1994 <vickyvip81@gmail.com>
2024-03-28 16:32:56 +0530

    add post section

--- client/src/Components/DashSidebar.jsx
+++ client/src/Components/DashSidebar.jsx
@@ -1,9 +1,10 @@
 import { Sidebar } from "flowbite-react"
-import {HiArrowSmRight, HiUser} from 'react-icons/hi'
+import {HiArrowSmRight, HiDocumentText, HiUser} from 'react-icons/hi'
 import { Link, useLocation } from "react-router-dom"
 import { useState,useEffect } from "react"
 import {signoutSuccess } from '../redux/user/userSlice.js'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 
 
@@ -12,6 +13,7 @@ export default function DashSidebar() {
     const location=useLocation()
     const [tab,setTab]=useState(' ')
     const disPatch=useDispatch()
+    const{currentUser}=useSelector(state=>state.user)
     useEffect(()=>{
       const urlParams= new URLSearchParams(location.search)
       const tabFromUrl=urlParams.get('tab')
@@ -38,17 +40,28 @@ export default function DashSidebar() {
   return (
    <Sidebar className='w-full md:w-56 '>
     <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-4">
             <Link to={'/dashboard?tab=profile'}>
             <Sidebar.Item active ={tab ==='profile'} 
             icon={HiUser}
-             label={'user'} 
+             label={currentUser.isAdmin ? 'Admin':'User' } 
              labelColor={'dark'}
              as='div'
              >
                 profile
             </Sidebar.Item>
+            {currentUser.isAdmin && (
+                <Link to='/dashboard?tab=posts'>
+                <Sidebar.Item
+                active={tab==='posts'}
+                icon={HiDocumentText}
+                as='div'
+                > posts</Sidebar.Item>
+                
+                </Link>
 
+                ) }
+            
             <Sidebar.Item onClick={handleSignout} 
             icon={HiArrowSmRight}
              className='cursor-pointer'
