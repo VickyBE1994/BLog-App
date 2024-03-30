dfd3072d665cbe7d4a261cbc57fc0252201e3102
VickyBE1994 <vickyvip81@gmail.com>
2024-03-30 08:22:22 +0530

    create get users api routes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {updateUser,deleteUser,signout} from '../controllers/user.controller.js'
+import {updateUser,deleteUser,signout,getusers} from '../controllers/user.controller.js'
 import { veryfyToken } from '../utils/verifyUser.js'
 
 const router=express.Router()
@@ -8,6 +8,7 @@ const router=express.Router()
 router.put('/update/:userId',veryfyToken,updateUser)
 router.delete('/delete/:userId',veryfyToken,deleteUser)
 router.post('/signout',signout)
+router.get('/getusers',veryfyToken,getusers)
 
 
     
