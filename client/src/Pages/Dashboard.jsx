05faa2b290c0150c23e5d5be941c9b7569383562
VickyBE1994 <vickyvip81@gmail.com>
2024-03-30 09:36:15 +0530

    show user to the admin dashboard

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import{useLocation} from 'react-router-dom'
 import DashSidebar from '../Components/DashSidebar'
 import DashProfile from '../Components/DashProfile'
 import DashPosts from '../Components/DashPosts'
+import DashUsers from '../Components/DashUsers'
 
 export default function Dashboard() {
   const location=useLocation()
@@ -17,11 +18,15 @@ export default function Dashboard() {
   return (
 <div className='min-h-screen flex flex-col md:flex-row'>
   <div className='md:w-56'>
+    {/* sidebar */}
     <DashSidebar/>
   </div>
+  {/* profile */}
   {tab=== 'profile'&& <DashProfile/> }
   {/* posts */}
   {tab==='posts' && <DashPosts/>}
+  {/* users */}
+  {tab==='users' && <DashUsers/>}
 </div>
 
   );
